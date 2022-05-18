24b18d1baba5e7ff8a3c648f7868c8fded59642a
Alice Ribeiro <aliceoliveiraribeiro@gmail.com>
2022-05-17 22:23:57 -0300

    feat: :sparkles: implementa filtro de busca

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,6 +5,7 @@ import "./styles.css";
 import { Posts } from "../../components/Posts";
 import { loadPosts } from "../../utils/load-posts";
 import { Button } from "../../components/Button";
+import { TextInput } from "../../components/TextInput";
 
 class Home extends Component {
   state = {
@@ -12,6 +13,7 @@ class Home extends Component {
     allPosts: [],
     page: 0,
     postsPerPage: 3,
+    searchValue: "",
   };
 
   async componentDidMount() {
@@ -37,19 +39,40 @@ class Home extends Component {
     this.setState({ posts, page: nextPage });
   };
 
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({ searchValue: value });
+  };
+
   render() {
-    const { posts, page, postsPerPage, allPosts } = this.state;
+    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
     const noMorePosts = page + postsPerPage >= allPosts.length;
+
+    const filteredPosts = !!searchValue
+      ? allPosts.filter((post) => {
+          return post.title.toLowerCase().includes(searchValue.toLowerCase());
+        })
+      : posts;
 
     return (
       <section className="container">
-        <Posts posts={posts} />
-        <div className="button-container">
-          <Button
-            text="Carregar mais posts"
-            handleClick={this.loadMorePosts}
-            disabled={noMorePosts}
+        <div className="search-container">
+          {!!searchValue && <h1>Valor de pesquisa: {searchValue}</h1>}
+          <TextInput
+            handleChange={this.handleChange}
+            searchValue={searchValue}
           />
+        </div>
+        {filteredPosts.length > 0 && <Posts posts={filteredPosts} />}
+        {filteredPosts.length === 0 && <p>Não existem posts =(</p>}
+        <div className="button-container">
+          {!searchValue && (
+            <Button
+              text="Carregar mais posts"
+              handleClick={this.loadMorePosts}
+              disabled={noMorePosts}
+            />
+          )}
         </div>
       </section>
     );
